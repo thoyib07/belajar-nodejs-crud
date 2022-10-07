36f0ca1b1e3ac0fbc2d7ab97dcf2fb178844515e
Thoyib Hidayat <thoyib07@gmail.com>
2022-10-07 14:43:40 +0700

    implement all

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import dotenv from 'dotenv';
+"use static";
+
+const dotenv = require("dotenv");
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require('./models/index');
 
 
 const app = express();
@@ -14,9 +17,28 @@ var corsOption = {
 app.use(cors(corsOption));
 app.use(express.urlencoded({extended:true, limit:"10mb"}));
 app.use(express.json({limit:"10mb"}));
+require('./routes/tutorialRoute')(app);
+
+if (!process.env.IS_PROD) {
+    const seqOpt = (process.env.IS_PROD ? { force:true } : {});
+    db.conn.sync(seqOpt)
+        .then(() => {
+            console.info('Drop and re-sync db.');
+        })
+        .catch((err) => {
+            console.info(`Failed to sync DB : ${err.message}`);
+        });
+}
 
 app.get("/",(req, res) => {
     res.send("Selamat datang di belajar nodejs CRUD");
+});
+
+app.get("/test",(req, res) => {
+    // console.info(db.tutorial.findALL({
+    //     attribute: ['title']
+    // }));
+    console.info(db.tutorial);
 });
 
 app.listen(PORT, () => {
